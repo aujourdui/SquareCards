74eaed7cb5842218b1788f92990c4201b6241ba0
kyosuke ito <kyosuke.ito@gmail.com>
2021-09-25 22:29:33 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", function(){
+  window.open("https://www.youtube.com/watch?v=8I5UCTlMa34&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=9")
+})
+document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", function(){
+  window.open("https://www.youtube.com/watch?v=wtqbOFYivxw&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=11")
+})
+document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click", function(){
+  window.open("https://www.youtube.com/watch?v=H5tuwAO-_Kg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=12")
+})
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+  window.open("https://medium.com/@iamcodefoxx")
+})
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+  window.open("https://medium.com/@iamcodefoxx")
+})
+document.getElementsByClassName("article")[2].addEventListener("click", function(){
+  window.open("https://medium.com/@iamcodefoxx")
+})
